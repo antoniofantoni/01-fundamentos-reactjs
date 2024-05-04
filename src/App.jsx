7fdfed6f3225f8a0573a0e229d94a5a9f31cedee
Antonio Fantoni <antonio.b.fantoni@gmail.com>
2024-05-04 16:16:31 -0300

    commit: atualização nome author

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ export function App() {
         <Sidebar />
         <main>
           <Post
-            author="Antonio Braga Fantoni"
+            author="Antonio Fantoni"
             content="Lorem ipsdo, sit amet consectetur  elSu,xime."
           />
 
