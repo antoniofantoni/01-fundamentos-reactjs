99cb487ca1d3a9467efb451188f5a7534cc79353
Antonio Fantoni <antonio.b.fantoni@gmail.com>
2024-05-05 00:13:29 -0300

    commit: estyles-sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,20 @@
-
-import styles from './Sidebar.module.css'
+import styles from "./Sidebar.module.css";
+import fotoAntonio from '..//assets/foto-antonio.png'
 export function Sidebar() {
-	return (
-		<aside className={styles.sidebar}>
-			<img src='https://unsplash.com/pt-br/fotografias/controle-remoto-preto-na-mesa-vermelha-6sAl6aQ4OWI'/>
+  return (
+    <aside className={styles.sidebar}>
+      <img 
+			className={styles.cover}
+			src="https://images.unsplash.com/photo-1453060113865-968cea1ad53a?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
 
-
-		</aside>
-	)
+      <div className={styles.profile}>
+				<img className={styles.avatar} src={fotoAntonio}></img>
+        <strong>Antonio Fantoni</strong>
+				<span>Web developer</span>
+      </div>
+			<footer>
+				<a href="#">Editar seu perfil</a>
+			</footer>
+    </aside>
+  );
 }
-
