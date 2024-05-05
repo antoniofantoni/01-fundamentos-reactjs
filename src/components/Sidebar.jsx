41e72079a4e86bf1a996a2d959e3a569bc51210b
Antonio Fantoni <antonio.b.fantoni@gmail.com>
2024-05-05 01:05:43 -0300

    commit:foto

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,12 @@
 import styles from "./Sidebar.module.css";
 import fotoAntonio from '..//assets/foto-antonio.png'
+import foto from '../assets/foto.jpg'
 export function Sidebar() {
   return (
     <aside className={styles.sidebar}>
       <img 
 			className={styles.cover}
-			src="https://images.unsplash.com/photo-1453060113865-968cea1ad53a?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+			src={foto} />
 
       <div className={styles.profile}>
 				<img className={styles.avatar} src={fotoAntonio}></img>
