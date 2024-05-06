8ccc518b4458d2cc96249549643d04551df4e443
fantoni <antonio.b.fantoni@gmail.com>
2024-05-06 13:27:59 -0300

    commit

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,12 +1,18 @@
 import styles from "./Post.module.css";
+import fotoAntonio from "..//assets/foto-antonio.png";
 
 export function Post() {
   return (
     <article className={styles.post}>
       <header>
-        <div className={styles.author} >
-
+        <div className={styles.author}>
+          <img className={styles.avatar} src={fotoAntonio} />
+          <div className={styles.authorInfo}>
+            <strong>Antonio Fantoni</strong>
+            <span>Web developer</span>
+          </div>
         </div>
+        <time dateTime=""></time>
       </header>
     </article>
   );
